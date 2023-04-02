609683aafc3b1253d096187706388f860b0fbc52
shoaib <sk582511@gmail.com>
2023-04-02 16:27:02 +0530

    House pricing project set up

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+# from typing import LIST
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str):
+    with open(file_path,"r") as f:
+        requirements=f.readlines()
+        for i in range(len(requirements)):
+            requirements[i].replace("\n","")
+
+        if(HYPEN_E_DOT in requirements):
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+
+
+setup(
+
+    name ="RegressorProject",
+    version="0.0.1",
+    author="Shoaib khan",
+    author_email="sk582511@gmail.com",
+    install_requires=get_requirements("requirements.txt"),
+    packages=find_packages()
+)
